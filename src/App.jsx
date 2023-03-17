a40bd07ad4689e53d4006534f5362b5f1dae195b
Pedro Aznarez <pedroaznarez12@gmail.com>
2023-03-17 14:12:37 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css'
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
+function App() {
+	return (
+		<div className="App">
+			<NavBar />
+			<ItemListContainer greeting="Hola, Bienvenido a NG Games!" />
+		</div>
+	);
+}
+export default App;
